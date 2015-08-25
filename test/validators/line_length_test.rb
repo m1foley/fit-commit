c3236984134cde05459228d81ea925e502905231
Mike Foley <mike@polleverywhere.com>
2015-08-25 15:06:27 -0700

    Add more LineLength tests

--- test/validators/line_length_test.rb
+++ test/validators/line_length_test.rb
@@ -24,6 +24,26 @@ describe FitCommit::Validators::LineLength do
         assert_empty validator.warnings
       end
     end
+    describe "first line is over warning limit" do
+      let(:commit_msg) do
+        "x" * (FitCommit::Validators::LineLength::FIRST_LINE_MAX_LENGTH + 1)
+      end
+      it "has a warning" do
+        validator.validate
+        assert_empty validator.errors
+        assert_equal 1, validator.warnings[1].size
+      end
+    end
+    describe "first line is over error limit" do
+      let(:commit_msg) do
+        "x" * (FitCommit::Validators::LineLength::LINE_MAX_LENGTH + 1)
+      end
+      it "has an error and no warning" do
+        validator.validate
+        assert_equal 1, validator.errors[1].size
+        assert_empty validator.warnings
+      end
+    end
   end
   describe "second line" do
     describe "second line is not empty" do
@@ -31,6 +51,17 @@ describe FitCommit::Validators::LineLength do
       it "has error" do
         validator.validate
         assert_equal 1, validator.errors[2].size
+        assert_empty validator.warnings
+      end
+    end
+    describe "second line is not empty and too long" do
+      let(:commit_msg) do
+        "foo\n" + ("x" * (FitCommit::Validators::LineLength::LINE_MAX_LENGTH + 1))
+      end
+      it "only mentions blank error" do
+        validator.validate
+        assert_equal 1, validator.errors[2].size
+        assert_match(/must be blank/, validator.errors[2][0])
         assert_empty validator.warnings
       end
     end
@@ -42,8 +73,30 @@ describe FitCommit::Validators::LineLength do
         assert_empty validator.warnings
       end
     end
-    describe "second line is not present" do
+    describe "does not have a second line" do
       let(:commit_msg) { "foo" }
+      it "does not have error" do
+        validator.validate
+        assert_empty validator.errors
+        assert_empty validator.warnings
+      end
+    end
+  end
+  describe "body text" do
+    describe "line is over length limit" do
+      let(:commit_msg) do
+        "foo\n\n" + ("x" * (FitCommit::Validators::LineLength::LINE_MAX_LENGTH + 1))
+      end
+      it "has error" do
+        validator.validate
+        assert_equal 1, validator.errors[3].size
+        assert_empty validator.warnings
+      end
+    end
+    describe "line is equal to length limit" do
+      let(:commit_msg) do
+        "foo\n\n" + ("x" * FitCommit::Validators::LineLength::LINE_MAX_LENGTH)
+      end
       it "does not have error" do
         validator.validate
         assert_empty validator.errors
