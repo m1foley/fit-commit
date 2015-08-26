8e0c46d3650ee471b17987a07c8019a058dfd464
Mike Foley <mike@polleverywhere.com>
2015-08-25 22:33:45 -0700

    Add output test for Runner

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -50,10 +50,32 @@ describe FitCommit::Runner do
   end
 
   describe "commit msg contains errors" do
-    let(:commit_msg) { "foo\nbar" }
-    it "returns falsey value and prints to stdout" do
-      assert !call_runner
-      assert !stdout.read.empty?
+    let(:commit_msg) { "foo.\nbar" }
+
+    def assert_error_output
+      stdout_lines = stdout.read.lines.map(&:chomp)
+      assert_equal 7, stdout_lines.size
+      assert_equal commit_msg, stdout_lines[0..1].join("\n")
+      assert_empty stdout_lines[2]
+      assert_match(/\A1: Error: /, stdout_lines[3])
+      assert_match(/\A2: Error: /, stdout_lines[4])
+      assert_empty stdout_lines[5]
+      assert_equal "Force commit? [y/n] ", stdout_lines[6]
+    end
+
+    describe "user does not force commit" do
+      let(:stdin) { StringIO.new("n") }
+      it "prints errors to stdout and returns falsey value" do
+        assert !call_runner
+        assert_error_output
+      end
+    end
+    describe "user forces commit" do
+      let(:stdin) { StringIO.new("y") }
+      it "prints errors to stdout and returns truthy value" do
+        assert call_runner
+        assert_error_output
+      end
     end
   end
 end
